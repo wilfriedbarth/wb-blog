f22b5e800a8ed7181b6857491087e8149e592b37
Wilfried Barth <wilfried.barth.prof@gmail.com>
2019-02-08 21:15:31 -0600

    Remove offline and manifest plugins (#14)

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,19 +15,6 @@ module.exports = {
         trackingId: process.env.GA_TRACKING_ID,
       },
     },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        background_color: '#f7f0eb',
-        description: "Wilfried Barth's Blog",
-        display: 'minimal-ui',
-        name: 'WB Blog',
-        short_name: 'WB Blog',
-        start_url: '/',
-        theme_color: '#212472',
-      },
-    },
-    'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-typography',
